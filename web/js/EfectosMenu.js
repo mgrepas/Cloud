03ebd551304cd9a702421e44ec0b14d58ae94617
Victor Puello <Victor.puello@gmail.com>
2012-12-06 23:10:37 -0500

    Efectos

---
+++ web/js/EfectosMenu.js
@@ -0,0 +1,32 @@
+$(document).on("ready", iniciar);
+	function iniciar () {
+		$(".boxMPA").on("mouseover", animar)
+		$(".boxMPA").on("mouseout", mostrar)
+	}
+
+	function animar (dato) {
+		var Identificador = dato.currentTarget.id;
+		console.log(Identificador);
+		ocultar={
+			margin: "120% 0 0 0"
+		}
+		switch(Identificador){
+			case 'confSitema': 
+				$("#confSitema p").css(ocultar);
+			break;
+			default: break;
+		}	
+	}
+	function mostrar (dato) {
+		var Identificador = dato.currentTarget.id;
+		console.log(Identificador);
+		mostrar={
+			margin: "70% 0 0 0"
+		}
+		switch(Identificador){
+			case 'confSitema': 
+				$("#confSitema p").css(mostrar);
+			break;
+			default: break;
+		}	
+	}
