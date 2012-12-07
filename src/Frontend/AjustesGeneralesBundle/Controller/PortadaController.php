978dc3da6834a7e75c4e1aec9cd9e860e27986ed
Oscar Bertel <osber00@gmail.com>
2012-12-06 19:54:43 -0500

    Avances
    Ajax I - Menús

--- src/Frontend/AjustesGeneralesBundle/Controller/PortadaController.php
+++ src/Frontend/AjustesGeneralesBundle/Controller/PortadaController.php
@@ -6,7 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PortadaController extends Controller
 {
-    public function indexAction(){
+    public function portadaAction(){
         return $this->render('AjustesGeneralesBundle:Portada:portada.html.twig', array());
     }
+    public function menulateralAction(){
+        return $this->render('AjustesGeneralesBundle:Portada:menu_Lateral.html.twig', array());
+    }
+    public function menucoloresAction(){
+        return $this->render("AjustesGeneralesBundle:Portada:menuColores.html.twig", array());
+    }
+    
 }
--- web/js/ajax.js
+++ web/js/ajax.js
@@ -1,13 +1,26 @@
-$(document).on('ready',iniciar)
-
-function iniciar()
+$(window).load(doAjax);
+function doAjax()
 {
-    $('#ContAjax').on('click','.ajax',doAjax)
-    console.log('Soporte jQuery');
-}
-
-function doAjax(e)
-{
-	e.preventDefault();
-    alert('Implementar Ajax')
+    $('#ContAjax').on('click','.ajax',function(e){
+        e.preventDefault();
+        var enlace=$(this).attr('href');
+        //alert(enlace);
+        $.ajax({
+            url:enlace,
+            type:'get',
+            success:function(e){
+                $('#ContAjax').append("<div id='aux' style='display: none;' >"+e+"</div>");
+                var titulo=$('#aux').find('title').html();
+                var contenido=$('#aux').find('#tercernivel').html();
+                console.log(titulo);
+                console.log(contenido);
+                $('#aux').remove();
+                $('#ContAjax').html(contenido);
+                window.history.pushState(null, titulo, enlace);
+            },
+            error:function(){
+                alert('Error Ajax...')
+            }
+        })
+    })
 }
